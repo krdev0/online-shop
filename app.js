c6751209f52cdc9e49e2587854e865c4394ec881
Kristaps Ritins <kritinsdev@gmail.com>
2022-01-03 00:55:25 +0200

    start views

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const authRoutes = require('./routes/auth.routes');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(authRoutes)
 
 app.listen(3000);
