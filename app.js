15ffbf9c346b601387f90f6414fcc7d4ca5267a5
Kristaps Ritins <kritinsdev@gmail.com>
2022-01-07 19:55:00 +0200

    signup form

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const path = require('fs');
+const path = require('path');
 const express = require('express');
 
 const authRoutes = require('./routes/auth.routes');
@@ -7,6 +7,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
 
 app.use(authRoutes)
 
