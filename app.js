3996c3d2de6723c48ec2041a8c5b84e357c03e12
Kristaps Ritins <kritinsdev@gmail.com>
2022-01-09 23:30:06 +0200

    sessions implemented

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const csrf = require('csurf');
+const expressSession = require('express-session');
+const createSessionConfig = require('./config/session');
 
 const db = require('./data/database');
 const addCsrfTokenMiddleware = require('./middlewares/csrf-token');
@@ -13,6 +15,9 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: false }));
+
+const sessionConfig = createSessionConfig();
+app.use(expressSession(sessionConfig));
 app.use(csrf());
 app.use(addCsrfTokenMiddleware);
 
